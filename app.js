57638e97a84f17aea4eac42b0519ea3e81b54238
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-02 11:40:38 -0800

    Complete user registration

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ mongoose
 
 
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('This is working'));
 app.use('/api/users', users);
 app.use('/api/fups', fups);
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- models/User.js
+++ models/User.js
@@ -23,4 +23,4 @@ const UserSchema = new Schema({
   }
 })
 
-module.exports = User = monogoose.model('users', UserSchema);
+module.exports = User = mongoose.model('users', UserSchema);
