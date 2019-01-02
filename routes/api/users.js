2dbbf2b145f63675a923cb521511b9107fe4effe
your name here <theo@dali.dartmouth.edu>
2019-01-02 12:00:09 -0800

    remove logout timer

--- routes/api/users.js
+++ routes/api/users.js
@@ -52,8 +52,6 @@ router.post('/login', (req, res) => {
             jwt.sign(
               payload,
               keys.secretOrKey,
-              // Tell the key to expire in one hour
-              { expiresIn: 3600 },
               (err, token) => {
                 res.json({
                   success: true,
