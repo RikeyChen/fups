b71defd8e2348232744d5e03c6d5cd2290bc3bcc
Rikey Chen <rikey.chen16@gmail.com>
2019-01-02 11:05:55 -0800

    Initial commit

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>FUPS</h1>
+        <footer>
+          Copyright &copy; 2019 FUPS
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
