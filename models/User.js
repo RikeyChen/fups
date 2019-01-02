af0765d7276309cda3d06cccf4cb0dd72c63d104
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-02 11:19:46 -0800

    Create user model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true
+  },
+
+  password: {
+    type: String,
+    require: true
+  },
+
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = User = monogoose.model('users', UserSchema);
