8b05f87d2d7e469bb9dd52e461d22cd06a725d34
Rikey Chen <rikey.chen16@gmail.com>
2019-01-03 09:37:44 -0800

    Add styling to session form

--- frontend/src/components/session/login_form.jsx
+++ frontend/src/components/session/login_form.jsx
@@ -56,31 +56,35 @@ class LoginForm extends React.Component {
 
   render() {
     return (
-      <div className="session-form-container">
-        <h1>Welcome back!</h1>
-        <form onSubmit={this.handleSubmit}>
-          <div className="session-form">
-            <input type="text"
-              value={this.state.email}
-              onChange={this.update('email')}
-              placeholder="Email"
-            />
-            <br />
-            <input type="password"
-              value={this.state.password}
-              onChange={this.update('password')}
-              placeholder="Password"
-            />
-            <br />
-            <input type="submit" value="Log In" />
-            {this.renderErrors()}
-            <div>
-              <span>Don't have an account?</span>
-              {' '}
-              <Link to='/signup'>Sign Up</Link>
-            </div>
+      <div className="session-main">
+        <div className="session-form-container">
+          <div className="session-form-header">
+            <h1>Welcome back!</h1>
           </div>
-        </form>
+          <form onSubmit={this.handleSubmit}>
+            <div className="session-form">
+              <input type="text"
+                value={this.state.email}
+                onChange={this.update('email')}
+                placeholder="Email"
+              />
+              <br />
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                placeholder="Password"
+              />
+              <br />
+              <input type="submit" value="Log In" />
+              {this.renderErrors()}
+              <div>
+                <span>Don't have an account?</span>
+                {' '}
+                <Link to='/signup'>Sign Up</Link>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
