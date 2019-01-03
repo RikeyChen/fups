48f2e4859e8d0e97124af97415677510277a813c
Rikey Chen <rikey.chen16@gmail.com>
2019-01-02 22:07:48 -0800

    Update signup action creator to log the user in on successful sign up

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -24,12 +24,25 @@ export const logoutUser = () => ({
   type: RECEIVE_USER_LOGOUT
 });
 
+// export const signup = user => dispatch => (
+//   APIUtil.signup(user).then(() => (
+//     dispatch(receiveUserSignIn())
+//   ), err => (
+//     dispatch(receiveErrors(err.response.data))
+//   ))
+// );
+
 export const signup = user => dispatch => (
-  APIUtil.signup(user).then(() => (
-    dispatch(receiveUserSignIn())
-  ), err => (
-    dispatch(receiveErrors(err.response.data))
-  ))
+  APIUtil.signup(user).then(res => {
+    const { token } = res.data;
+    localStorage.setItem('jwtToken', token);
+    APIUtil.setAuthToken(token);
+    const decoded = jwt_decode(token);
+    dispatch(receiveCurrentUser(decoded))
+  })
+    .catch(err => {
+      dispatch(receiveErrors(err.response.data));
+    })
 );
 
 export const login = user => dispatch => (
