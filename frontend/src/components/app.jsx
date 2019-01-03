e30f4b2644e914a9c20b718dae0b27491a664a26
Rikey Chen <rikey.chen16@gmail.com>
2019-01-02 21:41:28 -0800

    Add profile component for testing

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -6,6 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import Profile from './profile/profile';
 
 const App = () => (
   <div>
@@ -14,6 +15,7 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
+      <ProtectedRoute exact path='/profile' component={Profile} />
     </Switch>
   </div>
 );
