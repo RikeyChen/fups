340835a02ab6a89928b7ede02c08c84683c4c216
Rikey Chen <rikey.chen16@gmail.com>
2019-01-03 14:36:25 -0800

    Add mapDispatchToProps and export

--- frontend/src/components/fups/fups_anonymous_container.js
+++ frontend/src/components/fups/fups_anonymous_container.js
@@ -1,6 +1,14 @@
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import FupsAnonymous from './fups_anonymous';
+import { fetchFups } from '../../actions/fups_actions';
 
 const mapStateToProps = state => ({
   fups: state.entities.fups
 })
+
+const mapDispatchToProps = dispatch => ({
+  fetchFups: () => dispatch(fetchFups())
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(FupsAnonymous));
