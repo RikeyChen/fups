029506513e937bdf13d7c7ea56ccb75796cbf226
your name here <theo@dali.dartmouth.edu>
2019-01-03 09:42:07 -0800

    Write function to determine the score of a fup

--- google_ai/api_filter.js
+++ google_ai/api_filter.js
@@ -8,27 +8,23 @@ const language = require('@google-cloud/language');
 // Instantiates a client
 const client = new language.LanguageServiceClient();
 
-// The text to analyze
-const text = 'I fucking hate my job';
+// Function to determine score of a fup 
+export const getFupScore = (fup) => {
+  let document = {
+    content: fup,
+    type: 'PLAIN_TEXT',
+  };
 
-const document = {
-  content: text,
-  type: 'PLAIN_TEXT',
+  client 
+    .analyzeSentiment({ document })
+    .then(results => {
+      const sentiment = results[0].documentSentiment;
+      return sentiment.score
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
 };
-
-// Detects the sentiment of the text
-client
-  .analyzeSentiment({ document: document })
-  .then(results => {
-    const sentiment = results[0].documentSentiment;
-
-    console.log(`Text: ${text}`);
-    console.log(`Sentiment score: ${sentiment.score}`);
-    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
 
   // Detects entities of the text 
 client
