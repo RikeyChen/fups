db93c0e68b490936dc503a74f4b1f18aae9028f4
your name here <theo@dali.dartmouth.edu>
2019-01-03 09:29:39 -0800

    test and work with google api filter

--- google_ai/api_filter.js
+++ google_ai/api_filter.js
@@ -5,7 +5,7 @@ const language = require('@google-cloud/language');
 const client = new language.LanguageServiceClient();
 
 // The text to analyze
-const text = 'Hello, world!';
+const text = 'I fucking hate my job';
 
 const document = {
   content: text,
@@ -24,4 +24,26 @@ client
   })
   .catch(err => {
     console.error('ERROR:', err);
+  });
+
+  // Detects entities of the text 
+client
+  .analyzeEntitySentiment({ document: document })
+  .then(results => {
+    
+    const entities = results[0].entities;        
+
+    console.log('Entities:');
+    entities.forEach(entity => {
+      console.log(entity.name);
+      console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+      console.log(' - Word Sentiment:');
+      console.log(` - Magnitude: ${entity.sentiment.magnitude}, Score: ${entity.sentiment.score}`);
+      
+      
+      
+      if (entity.metadata && entity.metadata.wikipedia_url) {
+        console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
+      }
+    })
   });
