ff2da03a0c4eb4d9c6ee9bdfd6b63d0025a585ad
Rikey Chen <rikey.chen16@gmail.com>
2019-01-03 12:40:57 -0800

    Add redirect route from / to signup

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import LoginFormContainer from './session/login_form_container';
@@ -11,6 +11,7 @@ const App = () => (
   <div id="app">
     <NavBarContainer />
     <Switch>
+      <Redirect from='/' exact to='/signup' />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <ProtectedRoute exact path='/profile' component={Profile} />
