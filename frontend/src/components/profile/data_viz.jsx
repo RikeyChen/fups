21fdf329fe49d1f62e04fe46612afc944e91aa26
your name here <theo@dali.dartmouth.edu>
2019-01-03 12:15:11 -0800

    Add data viz component

---
+++ frontend/src/components/profile/data_viz.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class DataViz extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div class_name='user_data_container'>
+
+      </div>
+    );
+  }
+}
+
+export default DataViz;
