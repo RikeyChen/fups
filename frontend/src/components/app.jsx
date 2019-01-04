f0a10c565e5c8a3462f8dd0f56c5f82f246067e2
Rikey Chen <rikey.chen16@gmail.com>
2019-01-03 19:44:10 -0800

    Change FupsAnonymous component to its container

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -6,7 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import Profile from './profile/profile';
-import FupsAnonymous from './fups/fups_anonymous';
+import FupsAnonymousContainer from './fups/fups_anonymous_container';
 
 const App = () => (
   <div id="app">
@@ -16,7 +16,7 @@ const App = () => (
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <ProtectedRoute exact path='/profile' component={Profile} />
-      <ProtectedRoute exact path='/fups' component={FupsAnonymous} />
+      <ProtectedRoute exact path='/fups' component={FupsAnonymousContainer} />
     </Switch>
   </div>
 );
