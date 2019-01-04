e27c245a3acfb6d0b366b051acb239dd4fda0aa2
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-04 09:51:38 -0800

    Add work in progress animation for pie chart

--- frontend/src/components/data_viz/pie.jsx
+++ frontend/src/components/data_viz/pie.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
 import ChartistGraph from 'react-chartist';
+import Chartist from 'chartist';
 
 class Pie extends React.Component {
   render() {
-    
+    const data = {
+      labels: ['Driving', 'Drinking', 'Money', 'Oversleeping', 'Boss', 'Wife'],
+      series: [5, 10, 5, 2, 10, 20]
+    }
+
+    const type = 'Pie';
+
+    const listener = {
+      'draw': data => {
+        if(data.type === 'slice') {
+          const pathLength = data.element._node.getTotalLength();
+
+          data.element.attr({
+            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+          });
+
+          const animation = {
+            'stroke-dashoffset': {
+              id: 'anim' + data.index,
+              dur: 1000,
+              from: -pathLength + 'px',
+              to: '0px',
+              easing: Chartist.Svg.Easing.easeOutQuint,
+              fill: 'freeze'
+            }
+          }
+          
+          if (data.index !== 0) {
+            animation['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+          }
+
+          data.element.attr({
+            'stroke-dashoffset': -pathLength + 'px'
+          });
+
+          data.element.animate(animation, false);
+        }
+      }
+    }
+
+    return(
+      <div>
+        <ChartistGraph data={data} listener={listener} type={type} />
+      </div>
+    )
   }
 }
+
+export default Pie;
