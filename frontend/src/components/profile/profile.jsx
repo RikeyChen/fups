5418fe7fd9c8efd405d52ecd4e20d0b179493962
your name here <theo@dali.dartmouth.edu>
2019-01-04 11:28:08 -0800

    Style graphs better

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -5,16 +5,22 @@ import Pie from '../data_viz/pie';
 import '../../stylesheets/profile.css';
 
 class Profile extends React.Component {
+
+  renderGraphs() {
+    return (
+      <div className='graphs_container'>
+        <Pie />
+        <Graph />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className='profile_page'>
         <div className='user_data_container'>
           <h1>Welcome back INSERT NAME OF USER</h1>
-          <div className='graphs_container'>
-            {/* this is where pie chart will go */}
-            <Graph />
-            <Pie />
-          </div>
+          {this.renderGraphs()}
         </div>
         <FupCreate />
       </div>
