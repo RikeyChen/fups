023f8dfb7c2daa274fe8ddcd91b785cba199fe08
Steven Le <steven.an.le@gmail.com>
2019-01-04 23:20:11 -0800

    Fix creating fups privately or publicly

--- frontend/src/components/fups/user_fups.jsx
+++ frontend/src/components/fups/user_fups.jsx
@@ -3,21 +3,6 @@ import FupsItem from './fups_item';
 import "../../stylesheets/fups_item.css"
 
 class UserFups extends React.Component {
-  constructor(props) {
-    super(props)
-
-    // this.state = {
-    //   fups: this.props.fups
-    // }
-  }
-
-  // componentWillMount() {
-  //   // this.props.fetchUserFups(this.props.currentUser.id)
-  // }
-
-  // componentWillReceiveProps(newState) {
-  //   this.setState({ fups: newState.fups });
-  // } 
 
   render() {
     return (
