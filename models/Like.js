f1c52469df72018d0c5dd0e01ef4e2a1f0462784
Rikey Chen <rikey.chen16@gmail.com>
2019-01-06 12:57:38 -0800

    Initial commit

---
+++ models/Like.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const LikeSchema = new Schema({
+  fup: {
+    type: Schema.Types.ObjectId,
+    ref: 'fups',
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Like = mongoose.model('like', LikeSchema);
