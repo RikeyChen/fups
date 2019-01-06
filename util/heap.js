6e3b506e46b28d173a83b3461a90672958103963
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-06 15:23:49 -0800

    Complete heapify up

--- util/heap.js
+++ util/heap.js
@@ -4,7 +4,7 @@ class BinaryMaxHeap {
   }
 
   extract() {
-    if (this.count() == 0) throw 'no element to extract';
+    if (this.count() === 0) throw 'no element to extract';
 
     let val = this.store[0];
 
@@ -24,29 +24,29 @@ class BinaryMaxHeap {
   }
 
   peek() {
-    if (this.count() == 0) throw 'no element to peek';
+    if (this.count() === 0) throw 'no element to peek';
     return this.store[0];
   }
 
   push(val) {
     this.store.push(val);
-    // heapify up
+    this.heapifyUp(this.store, this.count() - 1)
   }
 
   childIndicies(len, parent_idx) {
     [2 * parent_idx + 1, 2 * parent_idx + 2].filter(idx => idx < len)
   }
 
-  parent_index(child_idx) {
-    if (child_idx == 0) throw 'root has no parent';
-    let parent_idx = (child_idx - 1) / 2;
+  parentIndex(child_idx) {
+    if (child_idx === 0) throw 'root has no parent';
+    let parent_idx = Math.floor((child_idx - 1) / 2);
     return parent_idx;
   }
   
   compareNodes(a, b) {
     if (a > b) {
       return -1
-    } else if (a == b) {
+    } else if (a === b) {
       return 0
     } else {
       return 1
@@ -68,14 +68,35 @@ class BinaryMaxHeap {
 
     let swapIdx = null;
 
-    if (children.length == 1) {
+    if (children.length === 1) {
       swapIdx = leftIdx
     } else {
-      swapIdx = this.compareNodes(children[0], children[1]) == 1 ? leftIdx : rightIdx
+      swapIdx = func(children[0], children[1]) === 1 ? leftIdx : rightIdx
     }
 
     [array[swapIdx], array[parentIdx]] = [array[parentIdx], array[swapIdx]]
     this.heapifyDown(array, swapIdx, len, this.compareNodes);
+  }
+
+  heapifyUp(array, childIdx, len = array.length, func) {
+    func = func || this.compareNodes;
+
+    if (childIdx === 0) {
+      return array
+    }
+
+    let parentIdx = this.parentIndex(childIdx);
+
+    let [childVal, parentVal] = [array[childIdx], array[parentIdx]];
+
+    if (childVal <= parentVal) {
+      return array
+    }
+    let temp = array[childIdx];
+    array[childIdx] = array[parentIdx];
+    array[parentIdx] = temp
+    
+    this.heapifyUp(array, parentIdx, len, func)
   }
 
 }
