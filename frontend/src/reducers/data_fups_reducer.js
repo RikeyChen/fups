447d31a963918cbde2b284f9200931419a8e0da9
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-05 20:44:36 -0800

    DataFup slice of state fully functional

--- frontend/src/reducers/data_fups_reducer.js
+++ frontend/src/reducers/data_fups_reducer.js
@@ -1,10 +1,15 @@
-import { RECEIVE_DATA_FUPS } from '../actions/fups_actions';
+import { RECEIVE_DATA_FUPS, RECEIVE_NEW_FUP } from '../actions/fups_actions';
 
 const DataFupsReducer = (state = [], action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_DATA_FUPS:
       return action.fups;
+    case RECEIVE_NEW_FUP:
+      const newState = []
+      state.forEach(fup => newState.push(fup))
+      newState.push(action.fup)
+      return newState
     default:
       return state
   }
