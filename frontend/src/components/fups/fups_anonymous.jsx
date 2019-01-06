8bc741a053b500a2a2a8e1f546769156cb448b96
Rikey Chen <rikey.chen16@gmail.com>
2019-01-05 17:48:17 -0800

    Add styling to fups-item and upvote arrow

--- frontend/src/components/fups/fups_anonymous.jsx
+++ frontend/src/components/fups/fups_anonymous.jsx
@@ -13,15 +13,17 @@ class FupsAnonymous extends React.Component {
 
     return (
       <div className="fups-anonymous-container">
-        <h1>FUPSAnonymous</h1>
+        <h1 className="fups-anon-header">FUPS Anonymous</h1>
         {fups.map(fup => (
           <div className="fups-item-master">
-            <div>
+            <div className='anon-image-container'>
               <div className={`anon-image anon${fup.iconNum}`} />
             </div>
-            <div>
+            <div className="fups-item-main">
               <FupsItem fup={fup} key={fup._id} />
-              <div className="upvote-arrow" />
+              <div className='upvote-arrow-container'>
+                <div className="upvote-arrow" />
+              </div>
             </div>
           </div>
         ))}
