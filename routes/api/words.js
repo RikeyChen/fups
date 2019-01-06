6783f43793d1a784a3e0e0a8172cfe9a08a18480
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-05 19:45:30 -0800

    Word get returns word counter with filter

--- routes/api/words.js
+++ routes/api/words.js
@@ -4,7 +4,22 @@ const Word = require('../../models/Word');
 
 router.get('/:user_id', (req, res) => {
   Word.find({user: req.params.user_id})
-    .then(words => res.json(words))
+    .where('salience').gte(0.1)
+    .where('fupScore').lte(0)
+    .then(words => {
+      let counter = {}
+
+      // Create counter hash of words
+      words.forEach(word => {
+        if (counter[word.word]) {
+          counter[word.word] += 1
+        } else {
+          counter[word.word] = 1
+        }
+      })
+
+
+      res.json(counter)})
     .catch(err => res.status(404).json({ nowordsfound: 'No words found for this user'}))
 })
 
