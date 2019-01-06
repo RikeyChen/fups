4dae2b790ba4eff2b8d784d181c6bf4989385954
Steven Le <steven.an.le@gmail.com>
2019-01-06 12:21:36 -0800

    Fix merge issues

--- frontend/src/reducers/fups_reducer.js
+++ frontend/src/reducers/fups_reducer.js
@@ -1,14 +1,15 @@
-import {RECEIVE_FUPS, RECEIVE_USER_FUPS, RECEIVE_NEW_FUP, REMOVE_USER_FUPS} from '../actions/fups_actions';
+import {RECEIVE_FUPS, RECEIVE_USER_FUPS, RECEIVE_NEW_FUP, CLEAR_FUPS} from '../actions/fups_actions';
 
 const FupsReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_FUPS:
-      return action.fups;
+      let newState = state.slice();
+      return newState.concat(action.fups);
     case RECEIVE_USER_FUPS:
       return action.fups;
     case RECEIVE_NEW_FUP:
-      const newState = [];
+      newState = [];
       newState.push(action.fup);
       state.forEach(el => newState.push(el));
       return newState;
@@ -17,6 +18,6 @@ const FupsReducer = (state = [], action) => {
     default:
       return state;
   }
-}
+};
 
 export default FupsReducer;
