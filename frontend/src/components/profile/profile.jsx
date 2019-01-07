138c4b48f25ed0a637c16e367c408c75457f158e
Steven Le <steven.an.le@gmail.com>
2019-01-06 16:18:49 -0800

    Add infinite scroll

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -3,18 +3,24 @@ import FupCreate from "../fups/create_fup_container";
 import Graph from '../data_viz/graph';
 import Pie from '../data_viz/pie';
 import '../../stylesheets/profile.css';
+import FupsItem from "../fups/fups_item";
 import UserFups from "../fups/user_fups";
-import SuggestionsBox from '../data_viz/suggestions_box';
+import InfiniteScroll from "react-infinite-scroller";
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
-  } 
+    this.state = {
+      hasMore: true
+    };
+    this.handleLoadMore = this.handleLoadMore.bind(this);
+    this.fupsLengthDiff = true;
+  }
 
   // importantWords() {
   //   let wordCount = {}
-  //   let words = [] 
-    
+  //   let words = []
+
   //   this.props.words.forEach(word => {
   //     if (wordCount[word]) {
   //       wordCount[word] += 1
@@ -26,12 +32,18 @@ class Profile extends React.Component {
   //   for (key in wordCount) {
   //     if (wordCount[key] >)
   //   }
-  // }  
+  // }
 
   componentDidMount() {
-    this.props.fetchUserWords(this.props.currentUserId); 
-    this.props.fetchUserFups(this.props.currentUserId);
+    this.props.fetchUserWords(this.props.currentUserId);
+    this.props.fetchUserFups(this.props.currentUserId, 0);
     this.props.fetchDataFups(this.props.currentUserId);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.fups.length === prevProps.fups.length) {
+      this.fupsLengthDiff = false;
+    }
   }
 
   componentWillUnmount() {
@@ -39,31 +51,52 @@ class Profile extends React.Component {
     this.props.removeDataFups();
   }
 
+  handleLoadMore(page) {
+    const fupsLength = this.props.fups.length;
+    this.props.fetchUserFups(this.props.currentUserId, page);
+    if (fupsLength % 25 > 0 && fupsLength % 25 < 25) {
+      this.setState({ hasMore: false });
+    }
+  }
+
   renderGraphs() {
     if (this.props.fups) {
       return (
-        <div className='graphs_container'>
+        <div className="graphs_container">
           <Pie />
           <Graph fups={this.props.dataFups} />
         </div>
-      )
+      );
     }
   }
 
   render() {
-    return (
-      <div className='profile_page'>
-        <div className='upper_page'>
-          <SuggestionsBox/>
-          <div className='user_data_container'>
-            <h1>Welcome back to FUPS</h1>
-            {this.renderGraphs()}
+    const { fups } = this.props;
+    if (!(fups instanceof Array) || !fups.length) return null;
+    const items = (
+        fups.map((fup, idx) => (
+          <div className="user-fups-container" key={idx}>
+            <FupsItem key={fup._id} fup={fup} />
           </div>
+        ))
+    )
+    // < UserFups fups = { this.props.fups } />
+    return (
+      <div className="profile_page">
+        <div className="user_data_container">
+          <h1>Welcome back INSERT NAME OF USER</h1>
+          {this.renderGraphs()}
         </div>
         <FupCreate />
-        <UserFups fups={this.props.fups}/>
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.handleLoadMore}
+          hasMore={this.state.hasMore}
+        >
+          {items}
+        </InfiniteScroll>
       </div>
-    )
+    );
   }
 }
 
