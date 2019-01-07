f468f95a37bdc265917cefc036ac8baec10d6240
Rikey Chen <rikey.chen16@gmail.com>
2019-01-06 17:39:45 -0800

    Add unlikeFup axios request

--- frontend/src/util/fups_api_util.js
+++ frontend/src/util/fups_api_util.js
@@ -16,6 +16,10 @@ export const getDataFups = (id) => {
   return axios.get(`/api/fups/data/${id}`)
 }
 
-export const likeFup = (id, data) => {
-  return axios.post(`/api/fups/${id}/likes`, data)
+export const likeFup = (fup_id, data) => {
+  return axios.post(`/api/fups/${fup_id}/likes`, data)
+}
+
+export const unlikeFup = (fup_id, like_id) => {
+  return axios.delete(`/api/fups/${fup_id}/likes/${like_id}`)
 }
