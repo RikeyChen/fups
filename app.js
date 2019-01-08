c3724a5747cf33006608bd01b8229e9c229fc3fe
your name here <theo@dali.dartmouth.edu>
2019-01-08 11:06:16 -0800

    uncomment production env to run on heroku

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('frontend/build'));
-//   app.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   })
-// }
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
