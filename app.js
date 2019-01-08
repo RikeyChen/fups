4968a3d985ddba0c5ae6a1b975a55e2e95f97ab1
your name here <theo@dali.dartmouth.edu>
2019-01-08 13:04:47 -0800

    change order of things in app.js for testing

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
-
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
@@ -30,6 +23,13 @@ app.get('/', (req, res) => res.send('This is working'));
 app.use('/api/users', users);
 app.use('/api/fups', fups);
 app.use('/api/words', words);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
