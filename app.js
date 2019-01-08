b99e0a51c98ee4901a84efd9d9f80d88882e3cc1
your name here <theo@dali.dartmouth.edu>
2019-01-07 16:44:03 -0800

    trying to get heroku to work

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.use('/api/users', users);
 app.use('/api/fups', fups);
 app.use('/api/words', words);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
